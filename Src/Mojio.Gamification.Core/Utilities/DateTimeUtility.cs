7d6c88e6e1e6c47eb5f5702384a0496bd98dbdda
ht92 <ht92@live.ca>
2015-03-20 18:03:49 -0700

    1. Fix small UI bug with About Fragment layout 2. Remove unreferenced method

--- Src/Mojio.Gamification.Core/Utilities/DateTimeUtility.cs
+++ Src/Mojio.Gamification.Core/Utilities/DateTimeUtility.cs
@@ -2,17 +2,12 @@
 
 namespace Mojio.Gamification.Core
 {
-	public class DateTimeUtility
+	public static class DateTimeUtility
 	{
 		public static DateTime UTC_OFFSET = new DateTime(1970, 1, 1);
 		public static string FORMAT_MMMM_DD_YYYY_H_MM_TT = "MMMM dd, yyyy h:mm tt";
 		public static string FORMAT_MMM_DD_YYYY_H_MM_TT = "MMM dd, yyyy h:mm tt";
 		public static string FORMAT_MMMM_DD_YYYY = "MMMM dd, yyyy";
-
-		public static double GetCurrentTimestamp ()
-		{
-			return (DateTime.Now.Subtract (UTC_OFFSET)).TotalSeconds;
-		}
 	}
 }
 
